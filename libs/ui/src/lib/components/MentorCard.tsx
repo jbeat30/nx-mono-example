5c16ca7bd0dc0709eaecccfc5ac2e1213a7af3ac
jbeat <jbeat0630@gmail.com>
2025-01-10 17:11:17 +0900

    fix: 타입 캐스팅을 통해 빌드 오류 수정

--- libs/ui/src/lib/components/MentorCard.tsx
+++ libs/ui/src/lib/components/MentorCard.tsx
@@ -3,7 +3,7 @@ import MentorCardImage from '../../assets/images/mentor-card-sample.png';
 export function MentorCard() {
   return (
     <div>
-      <img src={MentorCardImage as string} alt="멘토 카드 샘플"/>
+      <img src={(MentorCardImage as unknown) as string} alt="멘토 카드 샘플"/>
     </div>
   );
 }

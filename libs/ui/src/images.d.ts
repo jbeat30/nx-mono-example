3ffdef91c116630252f01955c57879340d2e7159
jbeat <jbeat0630@gmail.com>
2025-01-13 16:05:36 +0900

    feature: define image types for React and Next.js
    - Ensure img tags recognize string paths in all environments
    - Update type declarations for images to support image imports in both React and Next.js
    - Modify `imageSrc` logic to correctly handle `src` property for Next.js image objects

--- libs/ui/src/images.d.ts
+++ libs/ui/src/images.d.ts
@@ -1,28 +1,63 @@
 declare module '*.png' {
-  const value: { src: string };
+  const value: {
+    src: string;
+    width?: number;
+    height?: number;
+    blurDataURL?: string;
+  };
   export default value;
 }
 declare module '*.jpg' {
-  const value: { src: string };
+  const value: {
+    src: string;
+    width?: number;
+    height?: number;
+    blurDataURL?: string;
+  };
   export default value;
 }
 declare module '*.jpeg' {
-  const value: { src: string };
+  const value: {
+    src: string;
+    width?: number;
+    height?: number;
+    blurDataURL?: string;
+  };
   export default value;
 }
 declare module '*.gif' {
-  const value: { src: string };
+  const value: {
+    src: string;
+    width?: number;
+    height?: number;
+    blurDataURL?: string;
+  };
   export default value;
 }
 declare module '*.svg' {
-  const value: { src: string };
+  const value: {
+    src: string;
+    width?: number;
+    height?: number;
+    blurDataURL?: string;
+  };
   export default value;
 }
 declare module '*.ico' {
-  const value: { src: string };
+  const value: {
+    src: string;
+    width?: number;
+    height?: number;
+    blurDataURL?: string;
+  };
   export default value;
 }
 declare module '*.webp' {
-  const value: { src: string };
+  const value: {
+    src: string;
+    width?: number;
+    height?: number;
+    blurDataURL?: string;
+  };
   export default value;
 }
--- libs/ui/src/lib/components/MentorCard.tsx
+++ libs/ui/src/lib/components/MentorCard.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import MentorCardImage from '../../assets/images/mentor-card-sample.png';
 export function MentorCard() {
-  const imageSrc:string = typeof MentorCardImage === 'object' ? MentorCardImage?.src : MentorCardImage;
+  const imageSrc: string = typeof MentorCardImage === 'object' && 'src' in MentorCardImage
+    ? (MentorCardImage as { src: string }).src
+    : MentorCardImage;
   return (
     <div>
       <img src={imageSrc} alt="멘토 카드 샘플"/>
